71ead46124e42cd047002db3f238bbbcc938c7a0
Михаил Левин <mikelevin@yandex-team.ru>
2022-03-20 20:55:48 +0300

    Lesson 7. Jenkins

--- src/test/java/com/geekbrains/test/AbstractTest.java
+++ src/test/java/com/geekbrains/test/AbstractTest.java
@@ -3,13 +3,18 @@ package com.geekbrains.test;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class AbstractTest {
 
     protected String getResourceAsString(String resource) throws IOException {
         InputStream inputStream = getClass().getResourceAsStream(resource);
-        byte[] bytes = inputStream.readAllBytes();
-        return new String(bytes);
+        Scanner in = new Scanner(inputStream);
+        StringBuilder s = new StringBuilder();
+        while (in.hasNextLine()) {
+            s.append(in.nextLine());
+        }
+        return s.toString();
     }
 
     protected File getFile(String resource) throws IOException {
